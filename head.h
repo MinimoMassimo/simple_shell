a65d1f8ce50df09c9b7149cae06152afed91c85b
luzinashikorina <luzabiniam01@gmail.com>
2022-12-19 18:59:50 +0300

    main

---
+++ head.h
@@ -0,0 +1,10 @@
+#ifndef HEAD_H
+#define HEAD_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+//for fork()
+#include <sys/types.h>
+
+#endif
---
+++ shell.c
@@ -0,0 +1,16 @@
+#include "head.h"
+
+/**
+ * main - reproduces a simple shell
+ * @ac: number of arguments passe
+ * @av: arguments 
+ * @env: the environments of the proces
+ * Return: 0 on success, -1 otherwise
+ */
+int main(__attribute__((unused)) int ac, char **av, char **env)
+{
+	char *argv[] = {"/bin/sh", NULL};
+
+	execve(argv[0], av + 1, NULL);
+	return (0);
+}
